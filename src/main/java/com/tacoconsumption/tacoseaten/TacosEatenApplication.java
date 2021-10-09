a55f23f017d25725cff3edbf6f11a802430ab94b
ALang-c7dg <anne.lang.c7dg@statefarm.com>
2021-10-09 12:54:18 -0500

    Initial commit

---
+++ src/main/java/com/tacoconsumption/tacoseaten/TacosEatenApplication.java
@@ -0,0 +1,13 @@
+package com.tacoconsumption.tacoseaten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacosEatenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacosEatenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tacoconsumption/tacoseaten/TacosEatenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacoconsumption.tacoseaten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacosEatenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
